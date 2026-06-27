0bf314c0961b320f3f4cc7e8fefcddf845289361
chon8282 <@gmail.com>
2026-06-27 20:58:35 +0900

    fix home minheight

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -131,8 +131,8 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-background">
-      <div className="container mx-auto px-4 pt-2 pb-6 max-w-md space-y-4">
+    <div className="bg-background">
+      <div className="container mx-auto px-4 pt-2 pb-6 max-w-md space-y-4">c
 
         <div>
           {currentSaju ? (
