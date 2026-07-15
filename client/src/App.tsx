dde2d7807c8f4eaeeeb7f573b2b083ec0933ec3f
chon8282 <@gmail.com>
2026-07-16 06:35:34 +0900

    스크롤 잘림 문제 수정 - main flex 제거, root min-height 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -242,7 +242,7 @@ function AppContent() {
             />
           )}
 
-          <main className="flex-1 flex flex-col min-h-0 pb-20 overflow-y-auto">
+          <main className="flex-1 min-h-0 pb-20 overflow-y-auto">
             <Router />
           </main>
           <BottomNavigation
